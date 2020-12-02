aeeb96c18edc724d783fe974fa3c7ae61bc0387d
Shirisha55 <75254089+Shirisha55@users.noreply.github.com>
2020-12-02 17:54:10 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,12 @@
+i=1
+while i<4:
+    a=input("enter user name")
+    b=input("enter passward")
+    i+=1
+    if a == "siri" and b == "2140":
+        print("successfully logged in")
+        break
+else:
+    print("attepmt maximam")
+    
+
