57bd796270279ec9e5ebecb110ce509407569d73
deepakr6242 <deepakr6242@gmail.com>
2020-12-03 04:21:30 +0000

    Review done

--- task2.py
+++ task2.py
@@ -8,5 +8,26 @@ while i<4:
         break
 else:
     print("attepmt maximam")
+
+    # i = 1
+
+# while i<4:
+#     a=input("enter user name")
+#     b=input("enter passward")
+#     i+=1
+#     if a == "siri" and b == "2140":
+#         print("successfully logged in")
+#         break
+#     else:
+#         print('enter correct username and pwd')
+# else:
+#     print("attepmt maximam")
+
+
+# # Enter space before and  after your operator
+# # have meaning varaibale name
+# # declare your variables  before the logic
+# # Do a lot of negative testing and  increase validations
     
+
 
